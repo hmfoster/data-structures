a4ee347e587a7871a72d04582e0579803d9be580
Hailey Foster <haileyhasmail@gmail.com>
2015-07-04 10:45:55 -0700

    Write hashTable data structure.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,42 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-};
+  //set this.i = tuple that contains the k,v pair.
+  if (this._storage[i]){
+    this._storage[i] = [this._storage[i]];
+    this._storage[i].push([k, v]);
+  } else {
+    this._storage[i] = [k, v];
+  }
 
+};
+// [this._storage[i]]
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //look at this._storage[i]
+  if (this._storage[i]){
+    if (Array.isArray(this._storage[i][0])){
+      //if the array is nested, search through it.
+      for (var j = 0; j < this._storage[i].length; j++){
+        if (this._storage[i][j][0] === k){
+          return this._storage[i][j][1];
+        }
+      }
+    } else {
+    return this._storage[i][1];
+    //return that value
+    }
+  } else {
+    return null;
+  }
 
 };
 
 HashTable.prototype.remove = function(k){
-
+  //find where the key is
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  //delete the tuple at i
+  delete this._storage[i];
 };
 
 
